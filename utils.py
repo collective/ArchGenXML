4947c1a6734b3e849af5bae9a530ab81a9fe1ee3
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2004-11-23 13:16:46 +0000

    chnages conf ape-sopport to yesno
    svn path=/ArchGenXML/trunk/; revision=3343

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 import sys, os.path, time
 import getopt
 
@@ -54,7 +53,7 @@ def mapName(oldName):
 def readTemplate(fn):
     templdir=os.path.join(sys.path[0],'templates')
     templ=open(os.path.join(templdir,fn)).read()
-    
+
     return templ
 
 
@@ -64,7 +63,7 @@ def indent(s,indent,prepend='',skipFirstRow=0):
         lines=[rows[0]]+['    '*indent + prepend + l for l in rows[1:]]
     else:
         lines=['    '*indent + prepend + l for l in rows]
-        
+
     return '\n'.join(lines)
 
 def getExpression(s):
@@ -76,9 +75,9 @@ def getExpression(s):
 
     if s is None:
         s=''
-    
+
     s=s.strip()
-    
+
     if s and (s[0]=='"' and s[-1]=='"' or s[0]=="'" and s[-1]=="'"):
         return s
     else:
@@ -86,17 +85,17 @@ def getExpression(s):
             return s[7:]
         else:
             return "'''%s'''" % s
-        
+
 def isTGVTrue(tgv):
     if type(tgv) in (type(''),type(u'')):
         tgv=tgv.lower()
-        
+
     return tgv in (1,'1','true')
 
 def isTGVFalse(tgv):
     if type(tgv) in (type(''),type(u'')):
         tgv=tgv.lower()
-        
+
     return tgv in (0,'0','false')
 
 def toBoolean(v):
@@ -104,7 +103,7 @@ def toBoolean(v):
         v=v.lower()
 
     return v in (1,'1','true')
-    
+
 def cleanName(name):
     return name.replace(' ','_').replace('.','_').replace('/','_')
 
@@ -115,8 +114,8 @@ def wrap(text, width):
     A word-wrap function that preserves existing line breaks
     and most spaces in the text. Expects that existing line
     breaks are posix newlines (\n).
-    
-       
+
+
     """
     return reduce(lambda line, word, width=width: '%s%s%s' %
                   (line,
@@ -126,7 +125,7 @@ def wrap(text, width):
                    word),
                   text.split(' ')
                  )
-    
+
 # end code copy
 
 try:
@@ -134,7 +133,7 @@ try:
 except:
     from ConfigParser import ConfigParser
 
-    
+
 # key: (cmdlinekey?, shortcutkey?, cfg-file-key? -> 0 or section-name, internal settings-key, type)
 
 ## FIXME: ALL THIS OPTIONS NEED A MAJOR CLEANUP!
@@ -146,7 +145,7 @@ ALLOWED_OPTIONS_MAP = {
     'parse-packages=':                  (1,  0, 'GENERAL',        'parse_packages', 'commalist'),
     'force':                            (0, 'f', 'GENERAL',       'force', 'switchon'),
     'ape':                              (1, 0,   None,            'ape_support', 'switchon'),
-    'ape-support':                      (1, 0,   'STORAGE',       'ape_support', 'switchon'),
+    'ape-support':                      (1, 0,   'STORAGE',       'ape_support', 'yesno'),
     'actions':                          (1, 'a', None     ,       'generateActions', 'switchon'),
     'no-actions':                       (1, 0,   None,            'generateActions', 'switchoff'),
     'generate-actions':                 (0, 0,   'CLASSES',       'generateActions', 'yesno'),
@@ -171,7 +170,7 @@ ALLOWED_OPTIONS_MAP = {
     'license=':                         (1, 0,   'DOCUMENTATION', 'license', 'string'),
     'strip-html':                       (1, 0,   'DOCUMENTATION', 'striphtml', 'switchon'),
     'cfg=':                             (1, 'c', None,            None,'string'),
-    'project-configuration=':           (1, 0,   None,            None,'string'),                
+    'project-configuration=':           (1, 0,   None,            None,'string'),
     'storage=':                         (1, 0,   'GENERAL',       'storage', 'string'),
     'sql-storage-support':              (1, 0,   'CLASSES',       'sql_storage_support', 'switchon'),
     'default-field-generation':         (1, 0,   'CLASSES',       'default_field_generation', 'switchon'),
@@ -181,7 +180,7 @@ ALLOWED_OPTIONS_MAP = {
 }
 
 def set_setting(okey, value, settings):
-    """ set one option """ 
+    """ set one option """
     yesno={'yes':1, 'y':1, 1:1, '1':1, 'no':None, 'n':None, 0:None, '0':None}
     if ALLOWED_OPTIONS_MAP[okey][3]:
         if ALLOWED_OPTIONS_MAP[okey][4] == 'switchon':
@@ -199,7 +198,7 @@ def set_setting(okey, value, settings):
 
 def modify_settings(key, value, settings, shortkey=0):
     """ option is an 2-tuple, settings a dict """
-    okey= len(key)>2 and key[:2]=='--' and key[2:]    
+    okey= len(key)>2 and key[:2]=='--' and key[2:]
     if okey:
         if not ALLOWED_OPTIONS_MAP.has_key(okey) and ALLOWED_OPTIONS_MAP.has_key(okey+'='):
             okey+='='
@@ -211,14 +210,14 @@ def read_project_configfile(filename,settings):
     cp = ConfigParser()
     try:
         fname = open(filename,"r")
-    except:            
+    except:
         print ARCHGENXML_VERSION_LINE
         print "\nERROR: Can't open project configuration file '%s'!", filename
         sys.exit(2)
-            
+
     cp.readfp(fname)
-    fname.close()    
-    
+    fname.close()
+
     for key in ALLOWED_OPTIONS_MAP.keys():
         fkey = key[len(key)-1] == '=' and key[:len(key)-1] or key
         if cp.has_option(ALLOWED_OPTIONS_MAP[key][2], fkey):
@@ -228,11 +227,11 @@ def read_project_configfile(filename,settings):
 ##    y=['['+ALLOWED_OPTIONS_MAP[key][2]+']\n'+(key[len(key)-1] == '=' and key[:len(key)-1] or key) for key in ALLOWED_OPTIONS_MAP.keys() if ALLOWED_OPTIONS_MAP[key][2]]
 ##    y.sort()
 ##    for x in y print x
-  
+
 def read_project_settings(args):
-    """ reads options from args and return options array"""   
+    """ reads options from args and return options array"""
     # this should use sometimes the new advenced python2.3 parser
-    
+
     # set defaults
     settings={}
     settings['version']=version()
@@ -242,40 +241,40 @@ def read_project_settings(args):
     settings['licence'] = None
     settings['module_info_header'] = 1
     settings['detailled_creation_permissions'] = None
-    settings['widget_enhancement'] = None    
+    settings['widget_enhancement'] = None
     settings['outfilename'] = None
-    
+
     shortoptions = ':'.join([ ALLOWED_OPTIONS_MAP[optkey][1] \
         for optkey in ALLOWED_OPTIONS_MAP.keys() \
         if ALLOWED_OPTIONS_MAP[optkey][1]]
     )
     longoptions = [optkey for optkey in ALLOWED_OPTIONS_MAP.keys() \
         if ALLOWED_OPTIONS_MAP[optkey][0]]
-        
+
     opts, args = getopt.getopt(args, shortoptions,longoptions)
-    
+
     prefix = ''
-    
+
     # first run to get configfile
     for option in opts:
         if option[0] in ['--project-configuration','--cfg','-c'] and option[1]:
             print "Use configfile", option[1]
             read_project_configfile(option[1],settings)
-            
+
     # second run to overide with commandline parameters
-    for option in opts: 
+    for option in opts:
         modify_settings(option[0], option[1], settings)
         shortdict = { }
         x=[shortdict.update({ALLOWED_OPTIONS_MAP[key][1]:key}) \
             for key in ALLOWED_OPTIONS_MAP.keys() \
             if ALLOWED_OPTIONS_MAP[key][1]]
-                
+
         if len(option[0])>1 and option[0][0]=='-' and shortdict.has_key(option[0][1:]):
             modify_settings('--'+shortdict[option[0][1:]], option[1], settings, shortkey=1)
     return settings, args
 
 ARCHGENXML_VERSION_LINE = """\
-ArchGenXML %(version)s 
+ArchGenXML %(version)s
 (c) 2003 BlueDynamics GmbH, under GNU General Public License 2.0 or later
 """
 
@@ -289,12 +288,12 @@ Usage: ArchGenXML.py -o <target>|-c <configfile> [ options ] <xmi-source-file>
 
 OPTIONS:
     -o <target>
-        Output file directory path for data  representation classes. Last part 
+        Output file directory path for data  representation classes. Last part
         is used for internal directory namings.
-        
+
     -P <name1>,<name2>...
-        names of packages to parse in source file 
-        
+        names of packages to parse in source file
+
     -a --actions
         generates actions (default)
 
@@ -304,50 +303,50 @@ OPTIONS:
     --method-preservation
         methods in the target sources will be preserved (default)
 
-    --no-method-preservation 
+    --no-method-preservation
         methods in the target sources will be preserved
 
-    -t --unknown-types-as-string 
+    -t --unknown-types-as-string
         unknown attribute types will be treated as text
 
     --ape-support
         generate apeconf.xml and generators for ape (needs Archetypes 1.1+)
-        
+
     --i18n-support
-        support for i18NArchetypes. Attributes with a stereotype 'i18n' or a 
+        support for i18NArchetypes. Attributes with a stereotype 'i18n' or a
         taggedValue 'i18n' set to '1' are multilingual.
 
     --no-widget-enhancements
-        do not create widgets with default label, label_msgid, description, 
-        description_msgid and i18ndomain. 
-        
-    --no-message-catalog
-        do not automagically create msgid catalogs 
+        do not create widgets with default label, label_msgid, description,
+        description_msgid and i18ndomain.
 
-    --creation-permission=<perm> 
-        specifies which permission to create content default:Add [project] 
+    --no-message-catalog
+        do not automagically create msgid catalogs
+
+    --creation-permission=<perm>
+        specifies which permission to create content default:Add [project]
         content
 
     --detailled-creation-permissions=<boolean>
         seperate creation permissions per class, defaults to 'no'
 
-    --no-module-info-header      
+    --no-module-info-header
         do not generate module info header
 
-    --author=<string>            
-        set default author string for module info headers, taggedValue will 
+    --author=<string>
+        set default author string for module info headers, taggedValue will
         override this
 
     --e-mail=<string>
-        set default e-mail adress string for module info headers, taggedValue 
+        set default e-mail adress string for module info headers, taggedValue
         will override this
 
     --copyright=<string>
-        set default copyright string for module info headers, taggedValue will 
+        set default copyright string for module info headers, taggedValue will
         override this
 
     --licence=<string>
-        set default licence string for module info-headers, taggedValue will 
+        set default licence string for module info-headers, taggedValue will
         override this
 
     --strip-html
@@ -359,4 +358,3 @@ OPTIONS:
 def usage(returncode=-1):
     print USAGE_TEXT
     sys.exit(returncode)
-    
