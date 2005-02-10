b5c930325c72c19b22ed458f841efaa5be295665
Martin Aspeli <optilude@gmail.com>
2005-02-10 01:54:46 +0000

    Typo in comment
    svn path=/ArchGenXML/trunk/; revision=3569

--- templates/__init__.py
+++ templates/__init__.py
@@ -3,7 +3,7 @@
 # here:
 #
 #   - To set global configuration variables, create a file AppConfig.py. This
-#       will be imported in config.py, which in turns is imported in each
+#       will be imported in config.py, which in turn is imported in each
 #       generated class and in this file.
 #   - To perform custom initialisation after types have been registered, use
 #       the protected code section at the bottom of initialize().
