a76e63a9bf6d83c011f3beb3bf1f86354dc2a693
Reinout van Rees <reinout@vanrees.org>
2005-04-12 13:28:56 +0000

    SetupTestcase was missing the same Products.PRODUCTNAME in an import as GenericTestcase.
    svn path=/ArchGenXML/trunk/; revision=3947

--- templates/tests/SetupTestcase.py
+++ templates/tests/SetupTestcase.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
 from Products.PloneTestCase import PloneTestCase
-from <dtml-var "klass.getGenParents()[0].getQualifiedModuleName(klass.getPackage())"> import <dtml-var "klass.getGenParents()[0].getCleanName()">
+from Products.<dtml-var "klass.getPackage().getProductName()">.<dtml-var "klass.getGenParents()[0].getQualifiedModuleName(klass.getPackage())"> import <dtml-var "klass.getGenParents()[0].getCleanName()">
 
 class TestSetup(<dtml-var "klass.getGenParents()[0].getCleanName()">):
 
