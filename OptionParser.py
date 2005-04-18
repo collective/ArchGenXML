fbc727d437d7f9ee77d85c6cea17410ea48bf54e
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2005-04-18 15:40:40 +0000

    Whitespace cleanup
    svn path=/ArchGenXML/trunk/; revision=4004

--- OptionParser.py
+++ OptionParser.py
@@ -21,7 +21,7 @@ class AGXHelpFormatter(TitledHelpFormatter):
     """Help formatter for ArchGenXML parser."""
 
     # subclassed to handle YES|NO values
-    
+
     def format_option_strings (self, option):
         """Return a comma-separated list of option strings & metavariables."""
 
@@ -51,7 +51,7 @@ class AGXOption(Option):
 
     # Subclassed to handle YES|NO fields, commalist fields, and to handle
     # sample_config file action.
-    
+
     def check_yesno(option, opt, value):
         yesno={'yes':1, 'y':1, 1:1, '1':1, 'no':None, 'n':None, 0:None, '0':None}
 
@@ -60,13 +60,13 @@ class AGXOption(Option):
         else:
             choices=", ".join(map(repr, yesno.keys()))
             raise OptionValueError(
-                    "option %s: invalid choice: %r (choose from %s)" % 
+                    "option %s: invalid choice: %r (choose from %s)" %
                     (opt, value, choices))
 
-                    
+
     #def check_commalist(option, opt, value):
     #    return value
-                   
+
     def take_action(self, action, dest, opt, value, values, parser):
         """Perform option."""
 
@@ -83,7 +83,7 @@ class AGXOption(Option):
             values.ensure_value(dest, []).extend(value)
         else:
             Option.take_action(self, action, dest, opt, value, values, parser)
-        
+
     ATTRS=Option.ATTRS + ['section']
     TYPES=Option.TYPES + ('yesno','commalist')
     ACTIONS=Option.ACTIONS + ('sample_config','load_config',)
@@ -122,7 +122,7 @@ class AGXOptionParser(OptionParser):
                 add_help_option,
                 prog)
         self.config_filename=config_filename
-    
+
     def get_all_options(self):
         """Return all options, recursing into groups."""
 
@@ -180,7 +180,7 @@ class AGXOptionParser(OptionParser):
         fname.close()
 
         # Collect all options
-        
+
         options = self.get_all_options()
 
         # Walk through options, checking in config file
@@ -207,10 +207,10 @@ parser = AGXOptionParser(usage=usage, description=description, version="%prog 1.
 
 
 
-parser.add_option("-o", 
+parser.add_option("-o",
         "--outfile",
-        dest="outfilename", 
-        metavar="PATH", 
+        dest="outfilename",
+        metavar="PATH",
         help="Package directory to create",
         section="GENERAL",
         )
@@ -220,15 +220,15 @@ parser.add_option("-o",
 
 group = OptionGroup(parser, "Configuration File Options")
 
-group.add_option("-c", 
-        "--cfg", 
+group.add_option("-c",
+        "--cfg",
         dest="config_file",
         help="Use configuration file",
         action="load_config",
         type="string",
         )
 
-group.add_option( "--sample-config", 
+group.add_option( "--sample-config",
         help="View sample configuration file.",
         action="sample_config",
         )
@@ -242,9 +242,9 @@ parser.add_option_group(group)
 group = OptionGroup(parser, "Parsing Options")
 
 group.add_option("-t",
-        "--unknown-types-as-string", 
+        "--unknown-types-as-string",
         dest="unknownTypesAsString",
-        help="Unknown attribute types will be treated as strings", 
+        help="Unknown attribute types will be treated as strings",
         action="store_true",
         section="CLASSES",
         )
@@ -260,11 +260,11 @@ group.add_option("--generate-packages",
 # XXX: When would this be the right option to use, as opposed to
 # generate-packages, above? - WJB
 
-group.add_option("-P", 
+group.add_option("-P",
         "--parse-packages",
         type="commalist",
-        action="append", 
-        metavar="GENERAL", 
+        action="append",
+        metavar="GENERAL",
         dest="parse_packages",
         help="Name of packages to parse in source file (can specify several times)",
         section="GENERAL",
@@ -274,7 +274,7 @@ group.add_option("-P",
 # Not sure that this works in the current system.
 # utils.py/makeFile looks for force="ask", but not sure this is set
 
-group.add_option("-f", 
+group.add_option("-f",
         "--force",
         help="Overwrite files without asking?",
         default=1,
@@ -288,36 +288,36 @@ parser.add_option_group(group)
 
 group = OptionGroup(parser, "Generation Options")
 
-group.add_option("--widget-enhancement", 
-        dest="widget_enhancement", 
-        action="store_true", 
-        default=True, 
+group.add_option("--widget-enhancement",
+        dest="widget_enhancement",
+        action="store_true",
+        default=True,
         help="Do not create widgets with default label, label_msgid, description,"
              " description_msgid, and i18ndomain",
         section="CLASSES",
         )
 
-group.add_option("--no-widget-enhancement", 
-        dest="widget_enhancement", 
-        action="store_false", 
+group.add_option("--no-widget-enhancement",
+        dest="widget_enhancement",
+        action="store_false",
         help="Do not create widgets with default label, label_msgid, description,"
              " description_msgid, and i18ndomain",
         )
 
 
-group.add_option("-a", 
-        "--actions", 
+group.add_option("-a",
+        "--actions",
         dest="generateActions",
         default=True,
         help="Generate actions (default)",
         )
 
-group.add_option("--no-actions", 
+group.add_option("--no-actions",
         help="Do not generate actions",
         dest="generateActions",
         )
 
-group.add_option("--generate-actions", 
+group.add_option("--generate-actions",
         type='yesno',
         dest="generateActions",
         help="",
@@ -338,24 +338,24 @@ group.add_option("--customization-policy",
         section="GENERAL",
         )
 
-group.add_option("--strip-html", 
-        action="store_true", 
+group.add_option("--strip-html",
+        action="store_true",
         help="Strip HTML tags from documentation strings"
              " (for UML editors, such as Poseidon, which store HTML inside docs.)",
         section="DOCUMENTATION",
         )
 
 
-group.add_option("--method-preservation", 
-        help="Methods in the target source will be preserved (default)", 
+group.add_option("--method-preservation",
+        help="Methods in the target source will be preserved (default)",
         default=1,
         dest="method_preservation",
         action="store_true",
         section="CLASSES",
         )
 
-group.add_option("--no-method-preservation", 
-        help="Methods in the target source will not be preserved", 
+group.add_option("--no-method-preservation",
+        help="Methods in the target source will not be preserved",
         dest="method_preservation",
         action="store_false",
         section="CLASSES",
@@ -383,12 +383,12 @@ group.add_option("--message-catalog",
         section="I18N",
         )
 
-group.add_option("--no-message-catalog", 
-        dest="build_msgcatalog", 
+group.add_option("--no-message-catalog",
+        dest="build_msgcatalog",
         help="Do not automatically create msgid catalogs",
         )
 
-group.add_option("--i18n-support", 
+group.add_option("--i18n-support",
         help="Support for i18NArchetypes. Attributes with a stereotype of 'i18n'"
              " or taggedValue of 'i18n'=1 will be multilingual.",
         )
@@ -405,7 +405,7 @@ parser.add_option_group(group)
 #----------------------------------------------------------------------------
 # Module information options
 
-group = OptionGroup(parser, "Customization Options", 
+group = OptionGroup(parser, "Customization Options",
         "These options set the defaults for the module information headers."
         " They can be overriden by taggedValues."
         )
@@ -419,35 +419,35 @@ group.add_option("--module-info-header",
         section="DOCUMENTATION",
         )
 
-group.add_option("--no-module-info-header", 
-        dest="module_info_header", 
+group.add_option("--no-module-info-header",
+        dest="module_info_header",
         help="Do not generate module information header",
         action="store_false",
         )
 
-group.add_option("--author", 
+group.add_option("--author",
         help="Set default author value",
         section="DOCUMENTATION",
         )
 
 # Added US spelling of email as alternate
 
-group.add_option("--e-mail", 
-        "--email", 
+group.add_option("--e-mail",
+        "--email",
         dest="email",
         help="Set default email",
         section="DOCUMENTATION",
         )
 
-group.add_option("--copyright", 
+group.add_option("--copyright",
         help="Set default copyright",
         section="DOCUMENATION",
         )
 
 # Added US spelling of license as alternate
 
-group.add_option("--license", 
-        "--licence", 
+group.add_option("--license",
+        "--licence",
         help="Set default licence.",
         section="DOCUMENTATION",
         )
@@ -461,23 +461,23 @@ parser.add_option_group(group)
 
 group = OptionGroup(parser, "Permissions")
 
-group.add_option("--creation-permission", 
-        dest="creation_permission", 
+group.add_option("--creation-permission",
+        dest="creation_permission",
         help='Specifies permission to create content'
              ' (defaults to "Add [project] content.")',
         section="CLASSES",
         )
 
-group.add_option("--detailed-created-permissions", 
-        action="store_true", 
+group.add_option("--detailed-created-permissions",
+        action="store_true",
         help="Separate creation permissions per class (defaults to no)",
         section="CLASSES",
         )
 
 # This was a typo, and should be deprecated.
 
-group.add_option("--detailled-creation-permissions", 
-        action="store", 
+group.add_option("--detailled-creation-permissions",
+        action="store",
         type="yesno",
         help=SUPPRESS_HELP,
         section="CLASSES",
@@ -497,15 +497,15 @@ group.add_option("--storage",
         )
 
 
-group.add_option("--ape", 
-        dest="ape_support", 
-        action="store_true", 
+group.add_option("--ape",
+        dest="ape_support",
+        action="store_true",
         help="Generate configuration and generators for APE",
         )
 
-group.add_option("--ape-support", 
-        dest="ape_support", 
-        action="store", 
+group.add_option("--ape-support",
+        dest="ape_support",
+        action="store",
         type="yesno",
         help="Generate configuration and generators for APE",
         section="STORAGE",
