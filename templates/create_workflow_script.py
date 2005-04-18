3d158d00f7324be762b564f522c326b6078388b9
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2005-04-18 15:47:52 +0000

    added infoheader
    svn path=/ArchGenXML/trunk/; revision=4009

--- templates/create_workflow_script.py
+++ templates/create_workflow_script.py
@@ -1,4 +1,18 @@
-## Workflow scripts
+<dtml-let infoheader="atgenerator.getHeaderInfo(package)">
+""" Workflow Scripts for: <dtml-var "statemachine.getCleanName()"> """
+
+# <dtml-var "infoheader['copyright']">
+#
+<dtml-var "infoheader['date']"># Generator: ArchGenXML Version <dtml-var "infoheader['version']">
+#            http://sf.net/projects/archetypes/
+#
+# <dtml-var "infoheader['licence']">
+#
+__author__    = '''<dtml-var "infoheader['authorline']">'''
+__docformat__ = 'plaintext'
+__version__   = '$ Revision 0.0 $'[11:-2]
+</dtml-let>
+
 ## <dtml-var "parsedModule.functions.keys()">
 <dtml-in "statemachine.getAllTransitionActionNames()">
 <dtml-let actionname="_['sequence-item']">
