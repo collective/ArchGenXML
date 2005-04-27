53b6ff0b96efbd018b84e1ea5d7bd1d9a9a99885
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2005-04-27 23:28:40 +0000

    atvm 1.0 adaption
    svn path=/ArchGenXML/trunk/; revision=4146

--- codesnippets.py
+++ codesnippets.py
@@ -161,7 +161,7 @@ This program is free software; you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"""
 
-REGISTER_VOCABULARY_ITEM      = """registerVocabularyItem(%s, '%s')"""
+REGISTER_VOCABULARY_ITEM      = """registerVocabularyTerm(%s, '%s')"""
 REGISTER_VOCABULARY_CONTAINER = """registerVocabulary(%s)"""
 
 TEMPL_CONSTR_TOOL="""
