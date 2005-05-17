a82ad4bada340e392ebb2269d4c0b08d610d7f02
Reinout van Rees <reinout@vanrees.org>
2005-05-17 08:26:25 +0000

    One parameter too much
    svn path=/ArchGenXML/trunk/; revision=4211

--- templates/tests/SetupTestcase.py
+++ templates/tests/SetupTestcase.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
 from Products.PloneTestCase import PloneTestCase
-from <dtml-var "klass.getGenParents()[0].getQualifiedModuleName(forcePluginRoot=1)"> import <dtml-var "klass.getGenParents()[0].getCleanName()">
+from <dtml-var "klass.getGenParents()[0].getQualifiedModuleName(None,forcePluginRoot=1)"> import <dtml-var "klass.getGenParents()[0].getCleanName()">
 
 class TestSetup(<dtml-var "klass.getGenParents()[0].getCleanName()">):
 
