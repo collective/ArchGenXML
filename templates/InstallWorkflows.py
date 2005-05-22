1cd7ecd441559c625cd4282325fccb25bd22d83e
Fabiano Weimar dos Santos <xiru@xiru.org>
2005-05-22 22:05:30 +0000

    Some cosmetic modifications to avoid too much blank lines.
    svn path=/ArchGenXML/trunk/; revision=4250

--- templates/InstallWorkflows.py
+++ templates/InstallWorkflows.py
@@ -5,17 +5,14 @@ def installWorkflows(self,package,out):
     ''' '''
     productname='<dtml-var "package.getCleanName()">'
     wft=getToolByName(self,'portal_workflow')
-
     <dtml-in "package.getStateMachines()">
     <dtml-let sm="_['sequence-item']">
-        
+
     iwf=ExternalMethod('temp','temp',productname+'.'+'<dtml-var "generator.cleanName(sm.getName())">', 'create<dtml-var "generator.cleanName(sm.getName())">') 
     wf=iwf(self,'<dtml-var "generator.cleanName(sm.getName())">')
     wft._setObject('<dtml-var "generator.cleanName(sm.getName())">',wf)
     wft.setChainForPortalTypes( <dtml-var "repr(sm.getClassNames())">,wf.getId())
-    
     </dtml-let>
     </dtml-in>
-    
     
     return wft
