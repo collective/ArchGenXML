5dec4706ca29443e8049ca2c661df3591a8a4132
Reinout van Rees <reinout@vanrees.org>
2005-08-05 14:03:06 +0000

    Formatting, short names to long names and method-level comments
    svn path=/ArchGenXML/trunk/; revision=4672

--- templates/create_workflow.py
+++ templates/create_workflow.py
@@ -1,10 +1,11 @@
 <dtml-let infoheader="atgenerator.getHeaderInfo(package)">
-""" Workflow: <dtml-var "statemachine.getCleanName()">
+"""Workflow: <dtml-var "statemachine.getCleanName()">
 """
 
 # <dtml-var "infoheader['copyright']">
 #
-<dtml-var "infoheader['date']"># Generator: ArchGenXML Version <dtml-var "infoheader['version']">
+<dtml-var "infoheader['date']">
+# Generator: ArchGenXML Version <dtml-var "infoheader['version']">
 #            http://sf.net/projects/archetypes/
 #
 # <dtml-var "infoheader['licence']">
@@ -23,130 +24,131 @@ from Products.ExternalMethod.ExternalMethod import ExternalMethod
 
 productname = '<dtml-var "package.getCleanName()">'
 
-def setup<dtml-var "statemachine.getCleanName()">(self, wf):
-    """
-    <dtml-var "statemachine.getCleanName()"> Workflow Definition
+def setup<dtml-var "statemachine.getCleanName()">(self, workflow):
+    """Define the <dtml-var "statemachine.getCleanName()"> workflow.
     """
 <dtml-let additional_roles="statemachine.getAllRoles(ignore=['Owner','Manager','Member','Reviewer','Authenticated','Anonymous'])">
 <dtml-if "additional_roles">
     # add additional roles to portal
-    portal=getToolByName(self,'portal_url').getPortalObject()
-    data=list(portal.__ac_roles__)
+    portal = getToolByName(self,'portal_url').getPortalObject()
+    data = list(portal.__ac_roles__)
     for role in <dtml-var "additional_roles">:
         if not role in self.__ac_roles__:
             data.append(role)
-    portal.__ac_roles__=tuple(data)
+    portal.__ac_roles__ = tuple(data)
 </dtml-if>
 </dtml-let>
 
-    wf.setProperties(title='<dtml-var "statemachine.getCleanName()">')
+    workflow.setProperties(title='<dtml-var "statemachine.getCleanName()">')
 
 <dtml-var "generator.getProtectedSection(parsedModule,'create-workflow-setup-method-header',1)">
-
     # generation of worklists is not defined yet...
     ### XXX :-(
 
     for s in <dtml-var "repr(statemachine.getStateNames(no_duplicates = 1))">:
-        wf.states.addState(s)
+        workflow.states.addState(s)
 
     for t in <dtml-var "repr(statemachine.getTransitionNames(no_duplicates = 1))">:
-        wf.transitions.addTransition(t)
+        workflow.transitions.addTransition(t)
 
     for v in ['review_history', 'comments', 'time', 'actor', 'action']:
-        wf.variables.addVariable(v)
+        workflow.variables.addVariable(v)
 
     for p in <dtml-var "repr(statemachine.getAllPermissionNames())">:
-        wf.addManagedPermission(p)
+        workflow.addManagedPermission(p)
 
     ## Initial State
 
-    wf.states.setInitialState('<dtml-var "statemachine.getInitialState().getName()">')
+    workflow.states.setInitialState('<dtml-var "statemachine.getInitialState().getName()">')
 
     ## States initialization
 
 <dtml-in "[s for s in statemachine.getStates(no_duplicates = 1) if s.getName()]">
-    sdef = wf.states['<dtml-var "_['sequence-item'].getName()">']
-    sdef.setProperties(title="""<dtml-var "_['sequence-item'].getTitle(generator)">""",
-                       transitions=<dtml-var "repr([t.getName() for t in _['sequence-item'].getOutgoingTransitions()])">)
+    stateDef = workflow.states['<dtml-var "_['sequence-item'].getName()">']
+    stateDef.setProperties(title="""<dtml-var "_['sequence-item'].getTitle(generator)">""",
+                                  transitions=<dtml-var "repr([t.getName() for t in _['sequence-item'].getOutgoingTransitions()])">)
 <dtml-in "_['sequence-item'].getPermissionsDefinitions()">
-    sdef.setPermission('<dtml-var "_['sequence-item'].get('permission')">', <dtml-var "_['sequence-item'].get('acquisition')">, <dtml-var "_['sequence-item'].get('roles')">)
+    stateDef.setPermission('<dtml-var "_['sequence-item'].get('permission')">',
+                                  <dtml-var "_['sequence-item'].get('acquisition')">,
+                                  <dtml-var "_['sequence-item'].get('roles')">)
 </dtml-in>
 
 </dtml-in>
     ## Transitions initialization
 <dtml-in "[t for t in statemachine.getTransitions(no_duplicates = 1) if t.getName()]">
-    <dtml-let tran="_['sequence-item']">
-    <dtml-if "tran.getAction()">
+<dtml-let transition="_['sequence-item']">
+<dtml-if "transition.getAction()">
 
     ##creation of workflow scripts
-    for wf_scriptname in <dtml-var "repr(tran.getAction().getSplittedName(padding=0))">:
-        if not wf_scriptname in wf.scripts.objectIds():
-            wf.scripts._setObject(wf_scriptname,ExternalMethod(wf_scriptname, wf_scriptname,
+    for wf_scriptname in <dtml-var "repr(transition.getAction().getSplittedName(padding=0))">:
+        if not wf_scriptname in workflow.scripts.objectIds():
+            workflow.scripts._setObject(wf_scriptname,ExternalMethod(wf_scriptname, wf_scriptname,
                 productname + '.<dtml-var "statemachine.getCleanName()">_scripts',
                 wf_scriptname))
-    </dtml-if>
+</dtml-if>
 
-    tdef = wf.transitions['<dtml-var "tran.getName()">']
-    tdef.setProperties(title="""<dtml-var "tran.getTaggedValue('label') or tran.getName()">""",
-                       new_state_id="""<dtml-var "tran.getTargetStateName()">""",
-                       trigger_type=<dtml-var "tran.getTriggerType()">,
-                       script_name="""<dtml-var "tran.getBeforeActionName() or ''">""",
-                       after_script_name="""<dtml-var "tran.getAfterActionName() or ''">""",
-                       actbox_name="""<dtml-var "tran.getTaggedValue('label') or tran.getName()">""",<dtml-call name="atgenerator.addMsgid(tran.getTaggedValue('label') or tran.getName())">
-                       actbox_url="""""",
-                       actbox_category="""workflow""",
-                       props=<dtml-var "tran.getProps()">,
-                       )
-</dtml-let></dtml-in>
+    transitionDef = workflow.transitions['<dtml-var "transition.getName()">']
+    transitionDef.setProperties(title="""<dtml-var "transition.getTaggedValue('label') or transition.getName()">""",
+                                new_state_id="""<dtml-var "transition.getTargetStateName()">""",
+                                trigger_type=<dtml-var "transition.getTriggerType()">,
+                                script_name="""<dtml-var "transition.getBeforeActionName() or ''">""",
+                                after_script_name="""<dtml-var "transition.getAfterActionName() or ''">""",
+                                actbox_name="""<dtml-var "transition.getTaggedValue('label') or transition.getName()">""",<dtml-call name="atgenerator.addMsgid(transition.getTaggedValue('label') or transition.getName())">
+                                actbox_url="""""",
+                                actbox_category="""workflow""",
+                                props=<dtml-var "transition.getProps()">,
+                                )
+</dtml-let>
+</dtml-in>
 
     ## State Variable
-    wf.variables.setStateVar('review_state')
+    workflow.variables.setStateVar('review_state')
 
     ## Variables initialization
-    vdef = wf.variables['review_history']
-    vdef.setProperties(description="""Provides access to workflow history""",
-                       default_value="""""",
-                       default_expr="""state_change/getHistory""",
-                       for_catalog=0,
-                       for_status=0,
-                       update_always=0,
-                       props={'guard_permissions': 'Request review; Review portal content'})
+    variableDef = workflow.variables['review_history']
+    variableDef.setProperties(description="""Provides access to workflow history""",
+                              default_value="""""",
+                              default_expr="""state_change/getHistory""",
+                              for_catalog=0,
+                              for_status=0,
+                              update_always=0,
+                              props={'guard_permissions': 'Request review; Review portal content'})
 
-    vdef = wf.variables['comments']
-    vdef.setProperties(description="""Comments about the last transition""",
-                       default_value="""""",
-                       default_expr="""python:state_change.kwargs.get('comment', '')""",
-                       for_catalog=0,
-                       for_status=1,
-                       update_always=1,
-                       props=None)
+    variableDef = workflow.variables['comments']
+    variableDef.setProperties(description="""Comments about the last transition""",
+                              default_value="""""",
+                              default_expr="""python:state_change.kwargs.get('comment', '')""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
-    vdef = wf.variables['time']
-    vdef.setProperties(description="""Time of the last transition""",
-                       default_value="""""",
-                       default_expr="""state_change/getDateTime""",
-                       for_catalog=0,
-                       for_status=1,
-                       update_always=1,
-                       props=None)
+    variableDef = workflow.variables['time']
+    variableDef.setProperties(description="""Time of the last transition""",
+                              default_value="""""",
+                              default_expr="""state_change/getDateTime""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
-    vdef = wf.variables['actor']
-    vdef.setProperties(description="""The ID of the user who performed the last transition""",
-                       default_value="""""",
-                       default_expr="""user/getId""",
-                       for_catalog=0,
-                       for_status=1,
-                       update_always=1,
-                       props=None)
+    variableDef = workflow.variables['actor']
+    variableDef.setProperties(description="""The ID of the user who performed the last transition""",
+                              default_value="""""",
+                              default_expr="""user/getId""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
-    vdef = wf.variables['action']
-    vdef.setProperties(description="""The last transition""",
-                       default_value="""""",
-                       default_expr="""transition/getId|nothing""",
-                       for_catalog=0,
-                       for_status=1,
-                       update_always=1,
-                       props=None)
+    variableDef = workflow.variables['action']
+    variableDef.setProperties(description="""The last transition""",
+                              default_value="""""",
+                              default_expr="""transition/getId|nothing""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
     # XXX Generation of worklists is not implemented yet...
     # in the meantime, you can use the protected code section below to
@@ -155,7 +157,9 @@ def setup<dtml-var "statemachine.getCleanName()">(self, wf):
 <dtml-var "generator.getProtectedSection(parsedModule,'create-workflow-setup-method-footer',1)">
 
 def create<dtml-var "statemachine.getCleanName()">(self, id):
-    "..."
+    """Create the workflow for <dtml-var "package.getCleanName()">.
+    """
+    
     ob = DCWorkflowDefinition(id)
     setup<dtml-var "statemachine.getCleanName()">(self, ob)
     return ob
