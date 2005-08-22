5754441b24662e6ced756c2cc5d79062389e9ab0
Reinout van Rees <reinout@vanrees.org>
2005-08-22 15:15:58 +0000

    Added imports to class, package, model, portlet, tool
    svn path=/ArchGenXML/trunk/; revision=4794

--- TaggedValueSupport.py
+++ TaggedValueSupport.py
@@ -584,6 +584,9 @@ for category in ['model', 'package', 'class', 'tool', 'portlet']:
         tgvRegistry.addTaggedValue(category=category,
                                    tagname=tagname,
                                    explanation=explanation)
+    tagname = 'imports'
+    explanation = """A list of python import statements which will be placed at the top of the generated file. Use this to make new field and widget types available, for example. Note that in the generated code you will be able to enter additional import statements in a preserved code section near the top of the file. Prefer using the imports tagged value when it imports something that is directly used by another element in your model. You can have several import statements, one per line, or by adding several tagged values with the name 'imports'."""
+    tgvRegistry.addTaggedValue(category=category, tagname=tagname, explanation=explanation)
 
 
 if __name__ == '__main__':
