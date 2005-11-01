a27e9c3f465d64ca2538896627e880f2fd11408a
Reinout van Rees <reinout@vanrees.org>
2005-11-01 08:09:31 +0000

    Added dict to list of not-generated parents.
    svn path=/ArchGenXML/branches/agxbootstrap/; revision=5138

--- templates/interfaces.py
+++ templates/interfaces.py
@@ -2,7 +2,7 @@ from zope.interface import Interface, Attribute
 
 <dtml-in "classes">
 <dtml-let klass="_['sequence-item']">
-<dtml-let parents="[p for p in klass.getGenParents() if p.getCleanName() != 'object']">
+<dtml-let parents="[p for p in klass.getGenParents() if p.getCleanName() not in ['object', 'dict', 'list']]">
 class I<dtml-var "klass.getCleanName()">(<dtml-if "parents"><dtml-var "','.join(['I'+p.getCleanName() for p in parents])"><dtml-else>Interface</dtml-if>):
     """
     """
