49eb9e95662fe72d60d346c5886b66dadf58af82
huron_ <huron_@localhost>
2006-01-05 15:47:00 +0000

    Added new validation_expression_errormsg tgv in attribute actegory
    svn path=/ArchGenXML/trunk/; revision=5437

--- TaggedValueSupport.py
+++ TaggedValueSupport.py
@@ -625,6 +625,9 @@ tagname = 'validation_expression'
 explanation = """Use an ExpressionValidator and sets the by value given expression. """
 tgvRegistry.addTaggedValue(category=category, tagname=tagname, explanation=explanation)
 
+tagname = 'validation_expression_errormsg'
+explanation = """Sets the error message to the ExpressionValidator (use with validation_expression to define the validation expression to which this error message applies). """
+tgvRegistry.addTaggedValue(category=category, tagname=tagname, explanation=explanation)
 
 # widgets (not a separate category!)
 
