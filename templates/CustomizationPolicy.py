d12df63668129eda8505d093c0ca6800d0aa3f0b
Reinout van Rees <reinout@vanrees.org>
2006-01-05 15:16:01 +0000

    Spelling corrections. Also added a notice that this file isn't overwritten.
    svn path=/ArchGenXML/trunk/; revision=5435

--- templates/CustomizationPolicy.py
+++ templates/CustomizationPolicy.py
@@ -1,20 +1,23 @@
 <dtml-var "generator.generateModuleInfoHeader(package)">
+#
+# NOTE: archgenxml doesn't regenerate this file! You can tamper with
+# it to your heart's content.
+#
 from zLOG import LOG, INFO
 from Products.CMFPlone.Portal import addPolicy
 from Products.CMFPlone.CustomizationPolicy import DefaultCustomizationPolicy
 from config import PROJECTNAME
-
 
 class <dtml-var "package.getProductName()">CustomizationPolicy(DefaultCustomizationPolicy):
     """Make a custom Plone for <dtml-var "package.getProductName()">."""
 
     def customize(self, portal):
         """Custom customize method."""
-        # If the customization Policy id called inside an already existing
-        # portal, calling the DefaultCustomizationPolicy.customize is a
-        # problem
-        # ugly try catch arround works, but should somewhere in future be
-        # replaced by a test from which location we are calling.
+        # If the customization Policy is called inside an already
+        # existing portal, calling the
+        # DefaultCustomizationPolicy.customize is a problem. This ugly
+        # try/except works, but should somewhere in future be replaced
+        # by a test from which location we are calling.
         try:
             DefaultCustomizationPolicy.customize(self, portal)
         except:
@@ -37,8 +40,9 @@ class <dtml-var "package.getProductName()">CustomizationPolicy(DefaultCustomizat
         portal.portal_quickinstaller.installProduct(PROJECTNAME)
         get_transaction().commit(1)
 
-    # Define your own customize_#_name methods after this line
-
+    # Define your own customize_#_name methods after this
+    # line.
+    
 
 def register(context):
     addPolicy('<dtml-var "package.getProductName()">', <dtml-var "package.getProductName()">CustomizationPolicy())
