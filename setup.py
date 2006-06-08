a6f38b3084cd85dc102fccfde751e90062a73a45
Reinout van Rees <reinout@vanrees.org>
2006-06-08 08:12:17 +0000

    Added first go at setuptools usage
    svn path=/ArchGenXML/branches/reinout-cleanup/; revision=6571

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import ez_setup
+ez_setup.use_setuptools()
+
+import re
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='archgenxml',
+      version='1.6.a',
+      license='GPL',
+      description='Generates plone products from UML',
+      long_description="""
+      TODO
+      """,
+      keywords="zope plone UML",
+      author='Reinout van Rees',
+      author_email='reinout@vanrees.org',
+      url='http://plone.org/products/instance-manager',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      entry_points={
+        'console_scripts':[
+            'archgenxml = archgenxml.mainprogram:main'
+            ]
+        }
+      )
