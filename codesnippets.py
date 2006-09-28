ec3e9cdb90b558f34f7cb9d8e80563032e766b65
Reinout van Rees <reinout@vanrees.org>
2006-09-28 14:28:36 +0000

    Fixed the readme for skins
    svn path=/ArchGenXML/trunk/; revision=7047

--- codesnippets.py
+++ codesnippets.py
@@ -321,10 +321,12 @@ TEMPL_APECONFIG_END = """</configuration>"""
 READMELOWEST = """\
 Directory 'skins/%s':
 
-This skin layer has low priority, put unique templates and scripts here.
-
-I.e. if you to want to create own unique views or forms for your product, this
-is the right place."""
+Put your templates, css and javascript files in here. When first
+installed, this skin layer is added to the plone skin. It is added
+right below the 'custom' layer. Later, other products can move it a
+little bit down, but it'll always be above the plone skin layers. So:
+you can use it to overwrite plone stuff.
+"""
 
 CMFMEMBER_IMPORTS = """\
 # imports needed by CMFMember
