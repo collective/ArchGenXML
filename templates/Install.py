0a4cfcf30fa815c7a74580823cb66be8b50e63ee
Reinout van Rees <reinout@vanrees.org>
2007-05-07 09:10:01 +0000

    Fix for http://plone.org/products/archgenxml/issues/119, have installation of membrane follow the workflow install.
    svn path=/ArchGenXML/branches/1.5/; revision=8011

--- templates/Install.py
+++ templates/Install.py
@@ -233,20 +233,6 @@ def install(self, reinstall=False):
 </dtml-in>
 </dtml-if>
 </dtml-let>
-<dtml-let remembers="[cn for cn in generator.getGeneratedClasses(package) if cn.hasStereoType(generator.remember_stereotype)]">
-<dtml-if "remembers"> 
-    # Adds our types to MemberDataContainer.allowed_content_types
-    types_tool = getToolByName(self, 'portal_types')
-    act = types_tool.MemberDataContainer.allowed_content_types
-    types_tool.MemberDataContainer.manage_changeProperties(allowed_content_types=act+(<dtml-in remembers>'<dtml-var "_['sequence-item'].getCleanName()">', </dtml-in>))
-    # registers with membrane tool ...
-    membrane_tool = getToolByName(self, 'membrane_tool')
-<dtml-in "remembers">
-    membrane_tool.registerMembraneType('<dtml-var "_['sequence-item'].getCleanName()">')
-    # print >> out, SetupMember(self, member_type='<dtml-var "_['sequence-item'].getCleanName()">', register=<dtml-var "str(_['sequence-item'].getTaggedValue('register', False))">).finish()
-</dtml-in>
-</dtml-if>
-</dtml-let>
 
     # try to call a workflow install method
     # in 'InstallWorkflows.py' method 'installWorkflows'
@@ -263,6 +249,20 @@ def install(self, reinstall=False):
         print >>out,res or 'no output'
     else:
         print >>out,'no workflow install'
+<dtml-let remembers="[cn for cn in generator.getGeneratedClasses(package) if cn.hasStereoType(generator.remember_stereotype)]">
+<dtml-if "remembers"> 
+    # Adds our types to MemberDataContainer.allowed_content_types
+    types_tool = getToolByName(self, 'portal_types')
+    act = types_tool.MemberDataContainer.allowed_content_types
+    types_tool.MemberDataContainer.manage_changeProperties(allowed_content_types=act+(<dtml-in remembers>'<dtml-var "_['sequence-item'].getCleanName()">', </dtml-in>))
+    # registers with membrane tool ...
+    membrane_tool = getToolByName(self, 'membrane_tool')
+<dtml-in "remembers">
+    membrane_tool.registerMembraneType('<dtml-var "_['sequence-item'].getCleanName()">')
+    # print >> out, SetupMember(self, member_type='<dtml-var "_['sequence-item'].getCleanName()">', register=<dtml-var "str(_['sequence-item'].getTaggedValue('register', False))">).finish()
+</dtml-in>
+</dtml-if>
+</dtml-let>
 
 <dtml-if "[klass for klass in generator.getGeneratedClasses(package) if generator.getOption('use_workflow', klass, None) is not None and not klass.getStateMachines()]">
     #bind classes to workflows
