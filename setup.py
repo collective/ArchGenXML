6afd4bed92edfcffb32b1af99d56874efbc95bbe
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2007-09-07 15:09:58 +0000

    added debian build rules
    svn path=/ArchGenXML/trunk/; revision=8481

--- setup.py
+++ setup.py
@@ -9,12 +9,29 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='archgenxml',
-      version='1.6.b1',
+      version='2.0 alphadev',
       license='GPL',
-      description='Generates plone products from UML',
+      description='Generates Plone products from UML',
       long_description="""
-      Archgenxml generates plone products out of UML models, saving
-      you a lot of time and boilerplate code. Be lazy, use archgenxm!
+With ArchGenXML you can create working python code without writing one single 
+line of python. It is is a commandline utility that generates fully functional 
+Zope Products based on the Archetypes framework from UML models using XMI 
+(.xmi, .zargo, .zuml) files. The most common use case is to generate a set of 
+custom content types, possibly with a few tools, a CMFMember type and some 
+workflows thrown in.
+
+In practice, you draw your UML diagrams in a tool like Poseidon or ObjectDomain 
+which has the ability to generate XMI files. Once you are ready to test your 
+product, you run ArchGenXML on the XMI file, which will generate the product 
+directory. After generation, you will be able to install your product in Plone 
+and have your new content types, tools and workflows available.
+
+At present, round-trip support is not implemented: Custom code can't be 
+converted back into XMI (and thus diagams). However, you can re-generate 
+your product over existing code. Method bodies and certain "protected" code 
+sections will be preserved. This means that you can evolve your product's 
+public interfaces, its methods and its attributes in the UML model, without 
+fear of losing your hand-written code.
       """,
       keywords="zope plone UML",
       author='Reinout van Rees',
