26796a7d0dc5f2c474975108102f22b0ff12400a
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2007-09-12 13:23:35 +0000

    install.py to runallSteps
    svn path=/ArchGenXML/trunk/; revision=8585

--- src/archgenxml/templates/Install.py
+++ src/archgenxml/templates/Install.py
@@ -4,24 +4,17 @@ from Products.CMFCore.utils import getToolByName
 from Products.<dtml-var "package.getProductModuleName()">.config import PROJECTNAME
 
 def install(self, reinstall=False):
-    """ External Method to install <dtml-var "package.getProductModuleName()"> """
+    """External Method to install <dtml-var "package.getProductModuleName()"> 
+    
+    This method to install a product is kept, until something better will get
+    part of Plones front end, which utilize portal_setup.
+    """
     out = StringIO()
     print >> out, "Installation log of %s:" % PROJECTNAME
-    
-<dtml-let remembers="[cn for cn in generator.getGeneratedClasses(package) if cn.hasStereoType(generator.remember_stereotype)]">
-<dtml-if "remembers"> 
-    # Adds our types to MemberDataContainer.allowed_content_types
-    types_tool = getToolByName(self, 'portal_types')
-    act = types_tool.MemberDataContainer.allowed_content_types
-    types_tool.MemberDataContainer.manage_changeProperties(allowed_content_types=act+(<dtml-in remembers>'<dtml-var "_['sequence-item'].getCleanName()">', </dtml-in>))
-    # registers with membrane tool ...
-    membrane_tool = getToolByName(self, 'membrane_tool')
-<dtml-in "remembers">
-    membrane_tool.registerMembraneType('<dtml-var "_['sequence-item'].getCleanName()">')
-    # print >> out, SetupMember(self, member_type='<dtml-var "_['sequence-item'].getCleanName()">', register=<dtml-var "str(_['sequence-item'].getTaggedValue('register', False))">).finish()
-</dtml-in>
-</dtml-if>
-</dtml-let>
+
+    setuptool = getToolByName(self, 'portal_setup')
+    importcontext = 'profile-%s:default' % PROJECTNAME
+    setuptool.setImportContext(importcontext)
+    setuptool.runAllSteps()
 
     return out.getvalue()
-
