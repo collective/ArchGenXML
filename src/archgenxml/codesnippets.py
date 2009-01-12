f70aa7c18b0d48bffbb1e50f1b8bcdc624d0a3e7
sig <sig@localhost>
2009-01-12 21:20:07 +0000

    Added the GNU Affero General Public License (AGPL) to the list of allowed licenses.
    svn path=/ArchGenXML/trunk/; revision=10345

--- src/archgenxml/codesnippets.py
+++ src/archgenxml/codesnippets.py
@@ -158,6 +158,21 @@ LGPLTEXT = u"""\
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA."""
 
+AGPLTEXT = u"""\
+# This file is part of a program which is free software: you can
+# redistribute it and/or modify it under the terms of the
+# GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
 BSDTEXT = u"""\
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -213,6 +228,11 @@ LICENSES = {
     'LGPL': {
         'name': u'GNU Lesser General Public License (LGPL)',
         'text': LGPLTEXT,
+    },
+
+    'AGPL': {
+        'name': u'GNU Affero General Public License (AGPL)',
+        'text': AGPLTEXT,
     },
 
     'BSD': {
