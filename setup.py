8a82f2a673181465efacd1f48da484d82348902e
Vincent Fretin <vincent.fretin@gmail.com>
2010-09-04 16:06:30 +0000

    Back to development: 2.6
    svn path=/ArchGenXML/trunk/; revision=13045

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.5'
+version = '2.6dev'
 shortdesc = 'UML to code generator for Plone'
 
 long_description = (
